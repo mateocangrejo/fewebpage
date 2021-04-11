bdd89a9d19aab852197db45193af9f12e31d8b45
mateocangrejo <mateocangrejo@gmail.com>
2021-04-11 10:58:39 -0500

    Adición de lectio

--- scrapping.py
+++ scrapping.py
@@ -11,21 +11,25 @@ print('Registraste la fecha:', dt.year,'-',dt.month,'-',dt.day)
 #URL_ori_laudes = 'http://fraynelson.com/blog/2021/04/10/laudes-2021-04-11/'
 #URL_ori_visperas = 'https://fraynelson.com/blog/2021/04/10/visperas-2021-04-10/'
 #URL_ori_completas = 'https://fraynelson.com/blog/2021/04/10/completas-20210410/'
+#URL_ori_lectio = 'http://fraynelson.com/blog/2021/04/10/lectio-2021-04-11/'
 url_input_laudes = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day-1)
 url_input_visperas = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day)
 url_input_completas = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day)
+url_input_lectio = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day-1)
 
 #Url_Dates
 url_date_laudes = date
 url_date_visperas = date
 url_date_completas =  date.replace("-","")
+url_date_lectio = date
 
 #str(dt.year)+str(dt.month)+str(dt.day)
 
 dict_inputs={
     'laudes': [url_input_laudes,url_date_laudes],
     'visperas': [url_input_visperas,url_date_visperas],
-    'completas': [url_input_completas,url_date_completas]
+    'completas': [url_input_completas,url_date_completas],
+    'lectio': [url_input_lectio,url_date_lectio],
 }
 
 #urls_finales
@@ -38,7 +42,7 @@ for oracion, inputs_urls in dict_inputs.items():
 for oracion, url in dict_urls.items():
     print('Empezamos con: ',oracion)
     print('Accederemos a la url:',url)
-    print('Procesando ...................')
+    print('Procesando ...................\n')
     page = requests.get(url)
 
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -49,8 +53,13 @@ for oracion, url in dict_urls.items():
     body = soup.find('div', class_='nv-content-wrap entry-content')
 
     description = body.find_all('p')
+    youtube = body.find('iframe', class_='youtube-player')
+
     audio = body.find('a', class_='powerpress_link_d',href=True, title='Download')
 
     print(title_elem.text)
     print(description[0].text)
+    print(description[1].text)
     print(audio['href'])
+    if youtube is not None:
+        print(youtube['src'])
