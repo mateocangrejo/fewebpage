6e5561b0d89e73422a8f1b46e0255515674e0e3c
mateocangrejo <mateocangrejo@gmail.com>
2021-04-13 21:53:09 -0500

    correción en textos.py

--- textos.py
+++ textos.py
@@ -3,11 +3,29 @@ import requests
 from bs4 import BeautifulSoup
 
 
+date = input("Hola, registra una fecha en formato 2021/abr/10: ")
+#dt = datetime.strptime(date, '%Y/%m/%d')
+#print('Registraste la fecha:', dt.year,'-',dt.month,'-',dt.day)
+print('Registraste la fecha:', date)
 
-url='https://liturgiadelashoras.github.io/sync/2021/abr/11/laudes.htm'
+#URL_ori='https://liturgiadelashoras.github.io/sync/2021/abr/11/laudes.htm'
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-body = soup.find('div', id='cuerpo')
+url_base = 'https://liturgiadelashoras.github.io/sync/'
+dict_oraciones = {
+    'laudes': 'laudes',
+    'visperas': 'visperas',
+    'completas': 'completas',
+    'oficio': 'oficio'
+}
 
-print(body.text)
+for oracion, name in dict_oraciones.items():
+    url=url_base+date+'/'+oracion+'.htm'
+    print('............\n')
+    print('Accederemos al URL de',oracion,':',url)
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    body = soup.find('div', id='cuerpo')
+
+    print(body.decode_contents())
+    input('Siguiente, dale enter:')
