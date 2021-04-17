436873dcc5bb3af23f051d278477f1ff89ce2891
mateocangrejo <mateocangrejo@gmail.com>
2021-04-17 16:36:20 -0500

    crear funcion de textos.py que devuelve dict con los textos

--- textos.py
+++ textos.py
@@ -3,29 +3,46 @@ import requests
 from bs4 import BeautifulSoup
 
 
-date = input("Hola, registra una fecha en formato 2021/abr/10: ")
-#dt = datetime.strptime(date, '%Y/%m/%d')
-#print('Registraste la fecha:', dt.year,'-',dt.month,'-',dt.day)
-print('Registraste la fecha:', date)
 
-#URL_ori='https://liturgiadelashoras.github.io/sync/2021/abr/11/laudes.htm'
+def get_textos_liturgia(date):
+    #date = input("Hola, registra una fecha en formato 2021/abr/10: ")
+    dt = datetime.strptime(date, '%Y-%m-%d')
+    #print('Registraste la fecha:', dt.year,'-',dt.month,'-',dt.day)
+    meses = {
+        '1':'ene',
+        '2':'feb',
+        '3':'mar',
+        '4':'abr',
+        '5':'may',
+        '6':'jun',
+        '7':'jul',
+        '8':'ago',
+        '9':'sep',
+        '10':'oct',
+        '11':'nov',
+        '12':'dic',
+    }
+    date_str = str(dt.year)+'/'+meses[str(dt.month)]+'/'+str(dt.day)
 
-url_base = 'https://liturgiadelashoras.github.io/sync/'
-dict_oraciones = {
-    'laudes': 'laudes',
-    'visperas': 'visperas',
-    'completas': 'completas',
-    'oficio': 'oficio'
-}
+    #URL_ori='https://liturgiadelashoras.github.io/sync/2021/abr/11/laudes.htm'
+    url_base = 'https://liturgiadelashoras.github.io/sync/'
+    dict_oraciones = {
+        'laudes': 'laudes',
+        'visperas': 'visperas',
+        'completas': 'completas',
+        'oficio': 'oficio'
+    }
+    dict_salida = {}
+    for oracion, name in dict_oraciones.items():
+        url=url_base+date_str+'/'+oracion+'.htm'
+        print('............\n')
+        print('Accederemos al URL de',oracion,':',url)
 
-for oracion, name in dict_oraciones.items():
-    url=url_base+date+'/'+oracion+'.htm'
-    print('............\n')
-    print('Accederemos al URL de',oracion,':',url)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        body = soup.find('div', id='cuerpo')
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    body = soup.find('div', id='cuerpo')
-
-    print(body.decode_contents())
-    input('Siguiente, dale enter:')
+        dict_salida[oracion]=str(body.decode_contents())
+        print(body.decode_contents())
+        #input('Siguiente, dale enter:')
+    return dict_salida
--- update_sheets.py
+++ update_sheets.py
@@ -3,12 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 from sheets import read_sheets, write_sheets
 from lecturas_misa import get_lectura_misa
+from textos import get_textos_liturgia
 
 date = input("Hola, registra una fecha en formato 2021-04-10: ")
 
+"""
 lecutars_misa_dia = get_lectura_misa(date)
 
 range_write='Santifica el día!A22'
 list= [[date,lecutars_misa_dia]]
+value_range={'values':list}
+write_sheets(range_write, value_range)
+"""
+list = []
+dict_text_liturgia = get_textos_liturgia(date)
+for k,v in  dict_text_liturgia.items():
+    list_int = [date,k,v]
+    list.append(list_int)
+    #print(k)
+    #print('.................')
+    #print(v)
+range_write='Santifica el día!A22'
 value_range={'values':list}
 write_sheets(range_write, value_range)
