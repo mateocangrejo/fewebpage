342fed8d03c1e933482453073122c90927549dc4
mateocangrejo <mateocangrejo@gmail.com>
2021-04-17 19:47:07 -0500

    crear funcion para las oraciones de la liturgia y usarla en sheets

--- scrapping.py
+++ scrapping.py
@@ -3,62 +3,71 @@ import requests
 from bs4 import BeautifulSoup
 
 #Data Fecha
-date = input("Hola, registra una fecha en formato 2021-04-10: ")
-dt = datetime.strptime(date, '%Y-%m-%d')
-print('Registraste la fecha:', dt.year,'-',dt.month,'-',dt.day)
 
-#urls_inputs
-#URL_ori_laudes = 'http://fraynelson.com/blog/2021/04/10/laudes-2021-04-11/'
-#URL_ori_visperas = 'https://fraynelson.com/blog/2021/04/10/visperas-2021-04-10/'
-#URL_ori_completas = 'https://fraynelson.com/blog/2021/04/10/completas-20210410/'
-#URL_ori_lectio = 'http://fraynelson.com/blog/2021/04/10/lectio-2021-04-11/'
-url_input_laudes = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day-1)
-url_input_visperas = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day)
-url_input_completas = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day)
-url_input_lectio = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day-1)
+def get_oraciones_liturgia(date):
+    dt = datetime.strptime(date, '%Y-%m-%d')
+    #print('Registraste la fecha:', dt.year,'-',dt.month,'-',dt.day)
 
-#Url_Dates
-url_date_laudes = date
-url_date_visperas = date
-url_date_completas =  date.replace("-","")
-url_date_lectio = date
+    #urls_inputs
+    #URL_ori_laudes = 'http://fraynelson.com/blog/2021/04/10/laudes-2021-04-11/'
+    #URL_ori_visperas = 'https://fraynelson.com/blog/2021/04/10/visperas-2021-04-10/'
+    #URL_ori_completas = 'https://fraynelson.com/blog/2021/04/10/completas-20210410/'
+    #URL_ori_lectio = 'http://fraynelson.com/blog/2021/04/10/lectio-2021-04-11/'
+    url_input_laudes = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day-1)
+    url_input_visperas = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day)
+    url_input_completas = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day)
+    url_input_lectio = str(dt.year)+'/'+str(dt.month)+'/'+str(dt.day-1)
 
-#str(dt.year)+str(dt.month)+str(dt.day)
+    #Url_Dates
+    url_date_laudes = date
+    url_date_visperas = date
+    url_date_completas =  date.replace("-","")
+    url_date_lectio = date
 
-dict_inputs={
-    'laudes': [url_input_laudes,url_date_laudes],
-    'visperas': [url_input_visperas,url_date_visperas],
-    'completas': [url_input_completas,url_date_completas],
-    'lectio': [url_input_lectio,url_date_lectio],
-}
+    #str(dt.year)+str(dt.month)+str(dt.day)
 
-#urls_finales
-url_base='http://fraynelson.com/blog/'
+    dict_inputs={
+        'laudes': [url_input_laudes,url_date_laudes],
+        'visperas': [url_input_visperas,url_date_visperas],
+        'completas': [url_input_completas,url_date_completas],
+        'lectio': [url_input_lectio,url_date_lectio],
+    }
 
-dict_urls = {}
-for oracion, inputs_urls in dict_inputs.items():
-    dict_urls[oracion] = url_base+inputs_urls[0]+'/'+oracion+'-'+inputs_urls[1]+'/'
+    #urls_finales
+    url_base='http://fraynelson.com/blog/'
 
-for oracion, url in dict_urls.items():
-    print('\nEmpezamos con: ',oracion)
-    print('Accederemos a la url:',url)
-    print('Procesando ...................\n')
-    page = requests.get(url)
+    dict_urls = {}
+    for oracion, inputs_urls in dict_inputs.items():
+        dict_urls[oracion] = url_base+inputs_urls[0]+'/'+oracion+'-'+inputs_urls[1]+'/'
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    dict_salida = {}
+    for oracion, url in dict_urls.items():
+        #print('\nEmpezamos con: ',oracion)
+        #print('Accederemos a la url:',url)
+        #print('Procesando ...................\n')
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        try:
+            title_elem = soup.find('h1', class_='title entry-title')
+            body = soup.find('div', class_='nv-content-wrap entry-content')
+            description = body.find_all('p')
+            youtube = body.find('iframe', class_='youtube-player')
+            audio = body.find('a', class_='powerpress_link_d',href=True, title='Download')
+            dict_prov = {
+                'titulo': title_elem.text,
+                'texto': description[0].text,
+                'descripcion': description[1].text,
+                'audio': audio['href']
+            }
+            #print(title_elem.text)
+            #print(description[0].text)
+            #print(description[1].text)
+            #print(audio['href'])
+            if youtube is not None:
+                dict_prov['youtube']=youtube['src']
+                #print(youtube['src'])
+            dict_salida[oracion] = dict_prov
+        except:
+            continue
 
-    try:
-        title_elem = soup.find('h1', class_='title entry-title')
-        body = soup.find('div', class_='nv-content-wrap entry-content')
-        description = body.find_all('p')
-        youtube = body.find('iframe', class_='youtube-player')
-        audio = body.find('a', class_='powerpress_link_d',href=True, title='Download')
-
-        print(title_elem.text)
-        print(description[0].text)
-        print(description[1].text)
-        print(audio['href'])
-        if youtube is not None:
-            print(youtube['src'])
-    except:
-        continue
+    return dict_salida
--- update_sheets.py
+++ update_sheets.py
@@ -4,16 +4,19 @@ from bs4 import BeautifulSoup
 from sheets import read_sheets, write_sheets
 from lecturas_misa import get_lectura_misa
 from textos import get_textos_liturgia
+from scrapping import get_oraciones_liturgia
 
 date = input("Hola, registra una fecha en formato 2021-04-10: ")
 
+#lecturas de la misa
 """
 lecutars_misa_dia = get_lectura_misa(date)
-
 range_write='Santifica el día!A22'
 list= [[date,lecutars_misa_dia]]
 value_range={'values':list}
 write_sheets(range_write, value_range)
+"""
+#textos de la liturgia
 """
 list = []
 dict_text_liturgia = get_textos_liturgia(date)
@@ -26,3 +29,24 @@ for k,v in  dict_text_liturgia.items():
 range_write='Santifica el día!A22'
 value_range={'values':list}
 write_sheets(range_write, value_range)
+"""
+#datos de las oraciones de la liturgia
+list = []
+dict_oraciones_liturgia = get_oraciones_liturgia(date)
+for oracion,dict_prov in  dict_oraciones_liturgia.items():
+    list_int = [date, oracion]
+    for name, item in dict_prov.items():
+        #print('.................')
+        #print(name)
+        #print(item)
+        list_int.append(item)
+        #list.append(list_int)
+    list.append(list_int)
+range_write='Santifica el día!A22'
+value_range={'values':list}
+write_sheets(range_write, value_range)
+
+#update_night(date):
+#   update_laudes(date), update_lectio(date), update_gracia(date), update_homilia(date)
+#update_morning(date):
+#   update_visperas(date), update_completas(date)
